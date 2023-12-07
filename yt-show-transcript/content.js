72c582ca163f299bb9934ac4ff9654ccc8570b09
yichenlionelwu <lionelwuyichen@gmail.com>
2023-12-06 17:21:26 -0800

    gf patch v1

---
+++ yt-show-transcript/content.js
@@ -0,0 +1,31 @@
+waitForElm(".yt-spec-button-shape-next.yt-spec-button-shape-next--outline.yt-spec-button-shape-next--call-to-action.yt-spec-button-shape-next--size-m").then((elm) => {
+    console.log("element found and ready")
+    main()
+})
+
+function main() {
+    // click button to show transcript immediately
+    console.log("extension loaded")
+    var transcript = document.querySelector(".yt-spec-button-shape-next.yt-spec-button-shape-next--outline.yt-spec-button-shape-next--call-to-action.yt-spec-button-shape-next--size-m")
+    console.log(transcript)
+    transcript.click()
+}
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
