39430cfb3d5b25ece504ad82333162a252f8a9b5
l1ionel <yichenlionelwu@gmail.com>
2023-12-11 00:11:28 -0800

    updates to apclassroom logger

--- apclassroom-quiz-logger/index.js
+++ apclassroom-quiz-logger/index.js
@@ -1,5 +1,8 @@
 const render = document.getElementById("render")
+let db;
+let quiz_name;
 chrome.storage.local.get(null, function (result) {
+    db = result
     main(result)
 });
 
@@ -31,22 +34,25 @@ function handleButton(id) {
 
 function renderInfo(json) {
     console.log("rendering info")
-    render.innerHTML += `<h1>${json.MCQ_NAME}</h1>`
+    render.innerHTML += `<h1 id="quiz-name">${json.MCQ_NAME}</h1>`
     render.innerHTML += `<h2>Score: ${json.MCQ_SCORE}</h2>`
     render.innerHTML += `<h2> Started: ${json.MCQ_START_TIME} </h2>`
     render.innerHTML += `<h2> Finished: ${json.MCQ_END_TIME} </h2>`
     const questions = json.QUESTIONS
     const total_questions = questions.length
     for (let i = 0; i < questions.length; i++) {
+        console.log(i)
         var detail = questions[i]
-        render.innerHTML += `<h1>Question ${i + 1}/${total_questions}</h1>`
-        render.innerHTML += `<h3>${detail.IMAGE_TITLE == null ? "" : detail.IMAGE_TITLE}</h3>`
-        render.innerHTML += `<img style="max-width: 60%; margin: auto" src="${detail.IMAGE_URL}" alt="image">`
-        render.innerHTML += `<p style="font-size: large">${detail.QUESTION}</p>`
+        var question_number = `<div><h1>Question ${i + 1}/${total_questions}</h1>`
+        var image_title = `<h3>${detail.IMAGE_TITLE == null ? "" : detail.IMAGE_TITLE}</h3>`
+        var image_embed = detail.IMAGE_URL == null ? "" : `<img style="max-width: 60%; margin: auto" src="${detail.IMAGE_URL}" alt="image">`
+        var question = `<p style="font-size: large">${detail.QUESTION}</p>`
+        var question_choices = ""
         for (var x of detail.CHOICES) {
             const buttonId = (i + 1) + ":::" + x.charAt(7)
-            render.innerHTML += `<button id="${buttonId}">${x.slice(6)}</button><br><br>`
+            question_choices += `<button id="${buttonId}">${x.slice(6)}</button><br><br>`
         }
+        render.innerHTML += `<div id='question-${i + 1}'>` + question_number + image_title + image_embed + question + question_choices + "</div>"
     }
     buttonsEventListener()
 }
@@ -55,6 +61,14 @@ function buttonsEventListener() {
     const buttons = document.getElementsByTagName("button");
     const buttonPressed = e => {
         console.log(e.target.id);
+        var quiz_name = document.getElementById("quiz-name").innerText
+        var question_number = parseInt(e.target.id.split(":::")[0])
+        var question_choice = e.target.id.split(":::")[1]
+        console.log(question_number + " " + question_choice)
+        var question_details = db[quiz_name].QUESTIONS[question_number - 1]
+        if (question_details.ANSWER === question_choice) {
+            e.target.outerHTML += "<p>Correct answer!</p>"
+        }
     }
     for (let button of buttons) {
         button.addEventListener("click", buttonPressed);
