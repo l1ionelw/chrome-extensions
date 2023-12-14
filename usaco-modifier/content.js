b8f9fa9fc1361c5326d5a2e0eb53efc435443e8c
l1ionel <yichenlionelwu@gmail.com>
2023-12-13 21:29:04 -0800

    plan b

--- usaco-modifier/content.js
+++ usaco-modifier/content.js
@@ -1,20 +1,49 @@
-// Find all anchor tags on the page
-const links = document.getElementsById("a.masterTooltip");
-console.log(links);
-// Loop through all anchor tags
-for (let i = 0; i < links.length; i++) {
-  const link = links[i];
-  console.log(link[i]);
-  console.log(link);
-  console.log(link.getAttribute("class"));
-  // Check if the title attribute of the anchor tag is "Time Limit Exceeded"
-  if (link.title === "Time limit exceeded" || link.getAttribute("class") == "masterToolTip") {
-  console.log("found one");
-    // Find the closest ancestor div element with class "trial-result" and change its class to "trial-status-yes"
-    const ancestorDiv = link.closest(".trial-result");
-    if (ancestorDiv) {
-      ancestorDiv.classList.remove("trial-status-no");
-      ancestorDiv.classList.add("trial-status-yes");
+const url = window.location.href
+if (url === "http://www.usaco.org/index.php?page=viewproblem2&cpid=1155") {
+    console.log("changing question")
+    const data = {
+        "1": ["45.0mb", "69ms"],
+        "2": ["45.0mb", "69ms"],
+        "3": ["45.0mb", "69ms"],
+        "4": ["45.0mb", "69ms"],
+        "5": ["45.0mb", "69ms"],
     }
-  }
+    change_results(data)
+}
+
+function change_results(data) {
+    waitForElm(".masterTooltip").then(() => {
+        const trials = document.querySelectorAll(".masterTooltip")
+        for (var i = 0; i < trials.length; i++) {
+            var replace_value = data[i + 1]
+            if (replace_value === undefined) {
+                continue
+            }
+            var symbol = trials[i].querySelector(".res-symbol")
+            var info = trials[i].querySelector(".info").querySelectorAll("span")
+            symbol.innerText = "*"
+            info[0].innerText = replace_value[0]
+            info[1].innerText = replace_value[1]
+        }
+    })
+}
+
+
+
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
 }
