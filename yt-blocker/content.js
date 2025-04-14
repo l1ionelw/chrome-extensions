09bb26a4d04ff36c287d9c92f3afaf69eb0a63ed
l1ionelw <yichenlionelwu@gmail.com>
2025-04-14 10:09:04 -0700

    update yt blocker

--- yt-blocker/content.js
+++ yt-blocker/content.js
@@ -1,7 +1,7 @@
-window.onload = function() {
-    var contents = document.getElementById("contents");
-    if (contents) {
-      contents.parentNode.removeChild(contents);
-    }
-  }
-  
+let string = "<h1 style='color:black'>get to work little boy</h1><h1 style='color:white'>get to work little boy</h1>"
+window.onload = function () {
+  setInterval(() => {
+    var deleteAll = document.querySelector("body");
+    deleteAll.innerHTML = string;
+  }, 1000);
+}
